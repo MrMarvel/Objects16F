0e93edd2715af7a9638aecb42daeec34ea306f74
vv <u0NEEp9v>
2018-10-22 20:14:27 +0300

    Penguin's array

--- app/src/main/java/com/vv/objects/Penguin.java
+++ app/src/main/java/com/vv/objects/Penguin.java
@@ -6,14 +6,30 @@ import android.graphics.Paint;
 
 public class Penguin {
     float x , y;
+    float vx, vy;
     Paint paint = new Paint();
+
+    void step(){
+        this.x += vx;
+        this.y += vy;
+    }
 
     Penguin(float x, float y){
         this.x = x;
         this.y = y;
+        this.vx = (float)(Math.random()*4-2);
+        this.vy = (float)(Math.random()*4-2);
     }
+
+    Penguin(float x, float y, float vx, float vy){
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+
     void draw(Canvas canvas){
-         paint.setColor(Color.BLUE);
+        paint.setColor(Color.BLUE);
         canvas.drawCircle(x + 25, y + 90, 10, paint);
         canvas.drawCircle(x + 25,y + 45, 40, paint);
     }
--- app/src/main/java/com/vv/objects/WorldView.java
+++ app/src/main/java/com/vv/objects/WorldView.java
@@ -5,16 +5,30 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.view.View;
 public class WorldView extends View {
-    Penguin kesha = new Penguin(25, 100);
-    Penguin lusi = new Penguin(100, 100);
+    Penguin kesha;
+    Penguin lusi;
+    Penguin[] penguins;
     public WorldView(Context context) {
         super(context);
         this.setBackgroundColor(Color.BLACK);
+        kesha = new Penguin(25, 100);
+        lusi = new Penguin(100, 100, 2, 2);
+        penguins = new Penguin[10];
+        for (int i = 0; i <  penguins.length; i++){
+            penguins[i] = new Penguin(400, 600);
+        }
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        kesha.step();
+        lusi.step();
         kesha.draw(canvas);
         lusi.draw(canvas);
+        for (int i = 0; i <  penguins.length; i++){
+            penguins[i].draw(canvas);
+            penguins[i].step();
+        }
+        invalidate();
     }
 }
