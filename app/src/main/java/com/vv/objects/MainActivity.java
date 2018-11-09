727857f88523aed7230d56e3106ba64a9283bf47
vv <u0NEEp9v>
2018-11-09 18:05:24 +0300

    Android button

--- app/src/main/java/com/vv/objects/MainActivity.java
+++ app/src/main/java/com/vv/objects/MainActivity.java
@@ -2,13 +2,25 @@ package com.vv.objects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WorldView worldView;
+    final String TAG = "WORK";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        worldView = new WorldView(this);
-        this.setContentView(worldView);
+        this.setContentView(R.layout.activity_main);
+        worldView = findViewById(R.id.world_view);
+
+        Log.d(TAG, "onCreate");
+    }
+
+          public void plusPenguin(View view) {
+            worldView.penguins[worldView.n] =
+                    new Penguin(200, 200);
+            worldView.n++;
     }
 }
--- app/src/main/java/com/vv/objects/WorldView.java
+++ app/src/main/java/com/vv/objects/WorldView.java
@@ -3,11 +3,13 @@ package com.vv.objects;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.util.AttributeSet;
 import android.view.View;
 public class WorldView extends View {
     Penguin kesha;
     Penguin lusi;
     Penguin[] penguins;
+    int n = 10;
     public WorldView(Context context) {
         super(context);
         this.setBackgroundColor(Color.BLACK);
@@ -18,6 +20,19 @@ public class WorldView extends View {
             penguins[i] = new Penguin(400, 600);
         }
     }
+
+    public WorldView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setBackgroundColor(Color.BLACK);
+        kesha = new Penguin(25, 100);
+        lusi = new Penguin(100, 100, 2, 2);
+        penguins = new Penguin[100];
+        n = 10;
+        for (int i = 0; i < n; i++){
+            penguins[i] = new Penguin(400, 600);
+        }
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -25,7 +40,7 @@ public class WorldView extends View {
         lusi.step();
         kesha.draw(canvas);
         lusi.draw(canvas);
-        for (int i = 0; i <  penguins.length; i++){
+        for (int i = 0; i <  n; i++){
             penguins[i].draw(canvas);
             penguins[i].step();
         }
